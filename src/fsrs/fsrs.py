9980d8522c9bf097c744e5b92ba300dfc7863ad0
Jarrett Ye <jarrett.ye@outlook.com>
2023-12-01 12:09:03 +0800

    Feat/calc SInc based on last D (#19)

--- src/fsrs/fsrs.py
+++ src/fsrs/fsrs.py
@@ -61,13 +61,13 @@ class FSRS:
 
     def next_ds(self, s: SchedulingCards, last_d: float, last_s: float, retrievability: float):
         s.again.difficulty = self.next_difficulty(last_d, Rating.Again)
-        s.again.stability = self.next_forget_stability(s.again.difficulty, last_s, retrievability)
+        s.again.stability = self.next_forget_stability(last_d, last_s, retrievability)
         s.hard.difficulty = self.next_difficulty(last_d, Rating.Hard)
-        s.hard.stability = self.next_recall_stability(s.hard.difficulty, last_s, retrievability, Rating.Hard)
+        s.hard.stability = self.next_recall_stability(last_d, last_s, retrievability, Rating.Hard)
         s.good.difficulty = self.next_difficulty(last_d, Rating.Good)
-        s.good.stability = self.next_recall_stability(s.good.difficulty, last_s, retrievability, Rating.Good)
+        s.good.stability = self.next_recall_stability(last_d, last_s, retrievability, Rating.Good)
         s.easy.difficulty = self.next_difficulty(last_d, Rating.Easy)
-        s.easy.stability = self.next_recall_stability(s.easy.difficulty, last_s, retrievability, Rating.Easy)
+        s.easy.stability = self.next_recall_stability(last_d, last_s, retrievability, Rating.Easy)
 
     def init_stability(self, r: int) -> float:
         return max(self.p.w[r-1], 0.1)

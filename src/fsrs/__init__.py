49faa5e219ccf9881162d2364f10522b97c14155
Jarrett Ye <jarrett.ye@outlook.com>
2024-04-25 10:15:38 +0800

    Format/black

--- src/fsrs/__init__.py
+++ src/fsrs/__init__.py
@@ -2,7 +2,4 @@ __version__ = "1.2.0"
 
 from .fsrs import FSRS, Card
 
-from .fsrs import (
-    State,
-    Rating
-)
+from .fsrs import State, Rating
--- tests/test_fsrs.py
+++ tests/test_fsrs.py
@@ -16,13 +16,45 @@ def print_scheduling_cards(scheduling_cards):
 
 def test_repeat():
     f = FSRS()
-    f.p.w = (1.14, 1.01, 5.44, 14.67, 5.3024, 1.5662, 1.2503, 0.0028, 1.5489, 0.1763, 0.9953, 2.7473, 0.0179, 0.3105, 0.3976, 0.0, 2.0902)
+    f.p.w = (
+        1.14,
+        1.01,
+        5.44,
+        14.67,
+        5.3024,
+        1.5662,
+        1.2503,
+        0.0028,
+        1.5489,
+        0.1763,
+        0.9953,
+        2.7473,
+        0.0179,
+        0.3105,
+        0.3976,
+        0.0,
+        2.0902,
+    )
     card = Card()
     now = datetime(2022, 11, 29, 12, 30, 0, 0)
     scheduling_cards = f.repeat(card, now)
     print_scheduling_cards(scheduling_cards)
 
-    ratings = (Rating.Good, Rating.Good, Rating.Good, Rating.Good, Rating.Good, Rating.Good, Rating.Again, Rating.Again, Rating.Good, Rating.Good, Rating.Good, Rating.Good, Rating.Good)
+    ratings = (
+        Rating.Good,
+        Rating.Good,
+        Rating.Good,
+        Rating.Good,
+        Rating.Good,
+        Rating.Good,
+        Rating.Again,
+        Rating.Again,
+        Rating.Good,
+        Rating.Good,
+        Rating.Good,
+        Rating.Good,
+        Rating.Good,
+    )
     ivl_history = []
 
     for rating in ratings:
@@ -35,5 +67,6 @@ def test_repeat():
 
     print(ivl_history)
     assert ivl_history == [0, 5, 16, 43, 106, 236, 0, 0, 12, 25, 47, 85, 147]
+
 
 test_repeat()

5749b23068c03336ea87c5ee924c5d3233aeed04
Joshua Hamilton <hamiltonjoshuadavid@gmail.com>
2024-04-28 21:22:28 -0500

    add github action to automate pytest

--- tests/test_fsrs.py
+++ tests/test_fsrs.py
@@ -14,59 +14,58 @@ def print_scheduling_cards(scheduling_cards):
     print()
 
 
-def test_repeat():
-    f = FSRS()
-    f.p.w = (
-        1.14,
-        1.01,
-        5.44,
-        14.67,
-        5.3024,
-        1.5662,
-        1.2503,
-        0.0028,
-        1.5489,
-        0.1763,
-        0.9953,
-        2.7473,
-        0.0179,
-        0.3105,
-        0.3976,
-        0.0,
-        2.0902,
-    )
-    card = Card()
-    now = datetime(2022, 11, 29, 12, 30, 0, 0)
-    scheduling_cards = f.repeat(card, now)
-    print_scheduling_cards(scheduling_cards)
+class TestPyFSRS:
 
-    ratings = (
-        Rating.Good,
-        Rating.Good,
-        Rating.Good,
-        Rating.Good,
-        Rating.Good,
-        Rating.Good,
-        Rating.Again,
-        Rating.Again,
-        Rating.Good,
-        Rating.Good,
-        Rating.Good,
-        Rating.Good,
-        Rating.Good,
-    )
-    ivl_history = []
-
-    for rating in ratings:
-        card = scheduling_cards[rating].card
-        ivl = card.scheduled_days
-        ivl_history.append(ivl)
-        now = card.due
+    def test_repeat(self):
+        f = FSRS()
+        f.p.w = (
+            1.14,
+            1.01,
+            5.44,
+            14.67,
+            5.3024,
+            1.5662,
+            1.2503,
+            0.0028,
+            1.5489,
+            0.1763,
+            0.9953,
+            2.7473,
+            0.0179,
+            0.3105,
+            0.3976,
+            0.0,
+            2.0902,
+        )
+        card = Card()
+        now = datetime(2022, 11, 29, 12, 30, 0, 0)
         scheduling_cards = f.repeat(card, now)
         print_scheduling_cards(scheduling_cards)
 
-    print(ivl_history)
-    assert ivl_history == [0, 5, 16, 43, 106, 236, 0, 0, 12, 25, 47, 85, 147]
+        ratings = (
+            Rating.Good,
+            Rating.Good,
+            Rating.Good,
+            Rating.Good,
+            Rating.Good,
+            Rating.Good,
+            Rating.Again,
+            Rating.Again,
+            Rating.Good,
+            Rating.Good,
+            Rating.Good,
+            Rating.Good,
+            Rating.Good,
+        )
+        ivl_history = []
 
+        for rating in ratings:
+            card = scheduling_cards[rating].card
+            ivl = card.scheduled_days
+            ivl_history.append(ivl)
+            now = card.due
+            scheduling_cards = f.repeat(card, now)
+            print_scheduling_cards(scheduling_cards)
 
-test_repeat()
+        print(ivl_history)
+        assert ivl_history == [0, 5, 16, 43, 106, 236, 0, 0, 12, 25, 47, 85, 147]

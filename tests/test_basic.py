ab368e8ada3920082ee28ec4124a4b39c6f37896
Joshua Hamilton <hamiltonjoshuadavid@gmail.com>
2025-04-09 14:44:11 -0500

    add test to ensure card stability remains positive

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -718,3 +718,20 @@ class TestPyFSRS:
             card_ids.append(card_id)
 
         assert len(card_ids) == len(set(card_ids))
+
+    def test_positive_stability(self):
+        """
+        Ensure that a Card object's stability is always > 0
+        """
+
+        scheduler = Scheduler()
+
+        card = Card()
+
+        for _ in range(1000):
+            card, _ = scheduler.review_card(
+                card=card,
+                rating=Rating.Again,
+                review_datetime=card.due + timedelta(days=1),
+            )
+            assert card.stability > 0
